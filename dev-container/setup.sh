62505d8021d0a6546a97713dd8102bbf6a16ad0e
Stennsen <dennis@stennsen.dev>
2022-09-20 19:02:41 +0200

    moved codium extensions to setup.sh

--- dev-container/setup.sh
+++ dev-container/setup.sh
@@ -19,5 +19,14 @@ distrobox create -n code -H ~/Code -i code-container:latest
 echo "done"
 systemctl --user enable --now podman.socket
 distrobox enter code -- distrobox-export -a codium
-
+distrobox enter code -- codium --install-extension piousdeer.adwaita-theme
+distrobox enter code -- codium --install-extension svelte.svelte-vscode
+distrobox enter code -- codium --install-extension vscodevim.vim
+distrobox enter code -- codium --install-extension ms-python.python
+distrobox enter code -- codium --install-extension vscode.npm
+distrobox enter code -- codium --install-extension redhat.java
+distrobox enter code -- codium --install-extension redhat.vscode-xml
+distrobox enter code -- codium --install-extension redhat.vscode-yaml
+distrobox enter code -- codium --install-extension bradlc.vscode-tailwindcss
+distrobox enter code -- codium --install-extension Kelvin.vscode-sshfs
 echo "dev container setup finished"
